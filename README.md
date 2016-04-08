3c3301b99fd9b9d63eb0b1671e176d6ce4a46136
Ian Carroll <aidanogain@gmail.com>
2016-04-08 14:30:35 -0700

    add bug fixes and function abstraction:

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TemporalConverter
 Converts Gregorian Years to Sexagenary Stem-branch Cycle using JavaScript
 
-Additional features to be added:
+features to be added:
 - Convert Months, Days, Hours, Minutes, Senconds to Classical Chinese reckoning.
 - Convert elapsed time measurements
 - Convert spacial distances and other measurements.
--- StemBranchYearConverter.js
+++ StemBranchYearConverter.js
@@ -5,18 +5,28 @@ Returns the name of the corresponding Sexagenary Stem/Branch Cycle Year.
 The pure code minus html and side effects ==>
 */
 
-
+//todo:add in greater detail: Months, Days, Hours, Minutes, Seconds
+//todo:add time converter function in addition to date.
+/*
+Notes:
+relationships:
+1 year = 12 months = 36 xun = 360 days = 4320 shi = 34560 ke = 207360 fen = 20736000 miao
+1 month = 3 xun = 30 days = 360 shi = 2880 ke = 17280 fen = 1728000 miao
+1 xun = 10 days = 120 shi = 960 ke = 5760 fen = 576000 miao
+1 day = 12 shi = 96 ke = 576 fen = 57600 miao
+1 shi = 2 hours = 120 minutes = 7200 seconds = 8 ke = 48 fen = 4800 miao
+1 ke = 15 minutes = 900 seconds = 6 fen = 600 miao
+1 fen = 2.5 minutes = 150 seconds = 100 miao
+1 miao = 1.5 seconds
+*/
 
 // A switchboard conversion chart; Takes a %60 code number and returns a Sexagenary Stem/Branch name.
-function numberToStemBranch(number) {
-	stemNumber = number % 10;
-	branchNumber = number % 12;
+function numStemBranch(number) {
+	stemNum = number % 10;
+	branchNum = number % 12;
 	stem = "";
 	branch = "";
-	switch (stemNumber) {
-		case 0:
-			stem = "Gui";
-			break;	
+	switch (stemNum) {
 		case 1:
 			stem = "Jia";
 			break;
@@ -44,23 +54,11 @@ function numberToStemBranch(number) {
 		case 9:
 			stem = "Ren";
 			break;
-		case 10:
+		default:
 			stem = "Gui";
 			break;
-		case -1:
-			stem = "Ren";
-			break;
-		case -2:
-			stem = "Xin";
-			break;
-		default:
-			stem = "numberToStemBranch Error.";
-			break;
 	}
-	switch (branchNumber) {
-		case 0:
-			branch = "Hai";
-			break;	
+	switch (branchNum) {	
 		case 1:
 			branch = "Zi";
 			break;
@@ -94,24 +92,37 @@ function numberToStemBranch(number) {
 		case 11:
 			branch = "Xu";
 			break;
-		case 12:
+		default:
 			branch = "Hai";
 			break;
-		case -1:
-			branch = "Xu";
-			break;
-		case -2:
-			branch = "You";
-			break;
-		default:
-			stem = "numberToStemBranch Error.";
 	}
-	return stem + branch;
+	return stem + branch + "(" + number + ")";
 }
 
+//Massages the yearConverter number to get rid of those pesky negatives
+function numPure(rawNum) {
+	sixtyNum = 0
+	switch (rawNum) {
+	case 0:
+	    sixtyNum = 60;
+	    break;
+	case -1:
+	    sixtyNum = 59;
+	    break;
+	case -2:
+	    sixtyNum = 58;
+	    break;
+	case -3:
+	    sixtyNum = 60;
+	    break;
+	default:
+	    sixtyNum += rawNum;
+	    break;
+	}
+	return numStemBranch(sixtyNum);
+}
 
-
-// Controls the calibration of Gregorian Years to a %60 code number for numberToStemBranch to convert.
+// Controls the calibration of Gregorian Years to a %60 code number for numPure to massage the negaitves away and numStemBranch to convert.
 // Rounds and ignores finer date/time information coming in the form of floats to get "just the year at that time".
 // Accepts a single parameter (year) and assumes that to mean "AD" or "CE".
 // era input is non-case-sensitive.
@@ -121,10 +132,10 @@ function yearConverter(year, era) {
 		return "Please enter an Indian Numeral for the year without quotation marks.";
 	} else if ( era === undefined || (era.toUpperCase() === "AD" || era.toUpperCase() === "CE") && year >= 1) {
 		number = (year - 3) % 60;
-		return numberToStemBranch(Math.floor(number));
+		return numPure(Math.floor(number));
  	} else if ((era === "BC" || era === "BCE") && year >= 1) {
 		number = 60 - ((year + 2) % 60);
-		return numberToStemBranch(Math.ceil(number));
+		return numPure(Math.ceil(number));
 	} else if ((era !== "AD" && era !== "BC") && (era !== "CE" && era !== "BCE")) {
 		return "Please enter either \"AD\", \"BC\", \"CE\", or \"BCE\" for the era.";
 	} else {
