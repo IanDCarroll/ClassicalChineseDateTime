1c329f31237ddfa74fbf14ac31813dcfadb3c300
IanDCarroll <ian.d.carroll@gmail.com>
2016-04-20 17:45:36 -0700

    Add new function for year calculation

---
+++ SolarLunar.js
@@ -0,0 +1,7 @@
+/* solar & lunar carlulator
+ * this code returns the Chinese calendar year and month.
+ *    1. The new moon in ms relative to UTC 1/1/1970 00:00:00:000
+ *    2. The 24 solar terms of CCT as above.
+ * this data can then be passed to TempConV2.js to calculate the year, 
+ * month, xun, and day along with any intercalary months, and whether those  * months are long (30 days) or short (29) 
+ */
