fb5a2a15739913c4ad43809746b14cbc8c4d18e4
IanDCarroll <ian.d.carroll@gmail.com>
2016-04-23 22:33:20 -0700

    get synodic month function

--- SolarLunar.js
+++ SolarLunar.js
@@ -5,12 +5,15 @@
  * this data can then be passed to TempConV2.js to calculate the year, 
  * month, xun, and day along with any intercalary months, and whether those  * months are long (30 days) or short (29)
  *
- * Avg Synodic Month  = 	  2551442802 ms
- * Mean Tropical Year = 	 31556925000 ms
- * ms to next great Solar Term =  2629743750 ms
- * ms to next Solar Term = 	  1314871875 ms
+ * Avg Synodic Month  = 	   2551442802 ms
+ * Mean Tropical Year = 	  31556925000 ms
+ * ms to next great Solar Term =   2629743750 ms
+ * ms to next Solar Term = 	   1314871875 ms
+ *
  * Closest Winter Solstice to      UTC 1/1/1970 00:00:000 = 
- *	UTC 22/12/1969 00:44:00 = -861360000 ms
+ *	UTC 22/12/1969 00:44:000 = -861360000 ms
+ * Closest New Moon to 		   UTC 1/1/1970 00:00:000 =
+ *	UTC 07/01/1970 20:35:000 =  592500000 ms
  *
  * FEAR: Avg and Mean times may not be accurate enough, but what to do about * chaotic perturbations in the orbits of the celestial bodies? :/
  *
@@ -21,19 +24,20 @@
 
 //constants:
 var wSOffset = -861360000,
+    nMOffset =  592500000,
     mTropYr = 31556925000,
-    synodMo = 2551442802,
+    synodMo =  2551442802,
     SolTerm = mTropYr / 24,
     majSTrm = mTropYr / 12;
 
 //returns the time of solstice.
 function getSolstice(ms) {
-    var WinterSolsice = (ms + wSOffset) / mTropYr;
+    var winterSolsice = (ms + wSOffset) / mTropYr;
 }
 
 //Returns the new moon's wicked-cool beat.
 function getNewMoon(ms) {
-	//code
+    var newMoon = (ms + nMOffset) / synodMo;
 }
 
 //returns the CCYear
